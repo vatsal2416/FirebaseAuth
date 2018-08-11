108c3a5084bb81c4f1bfd138493ac7b0f005b824
vatsal2416 <vatsal2416@gmail.com>
2018-08-11 21:26:29 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/vatsal2416/firebaseauth/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.vatsal2416.firebaseauth;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends Activity {
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final TextView textEmail = (TextView) findViewById(R.id.et1);
+        final TextView textPassword= (TextView)findViewById(R.id.et2);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize buttons
+        Button btn_Login = findViewById(R.id.login);
+        Button btn_regsiter = findViewById(R.id.register);
+
+        //intialize Firebase Authentication
+        mAuth = FirebaseAuth.getInstance();
+
+        btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                methodLogin(textEmail.getText().toString(),textPassword.getText().toString());
+            }
+        });
+
+
+        btn_regsiter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                methodRegister(textEmail.getText().toString(),textPassword.getText().toString());
+            }
+        });
+    }
+
+    public void methodRegister(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Authentication Successful", Toast.LENGTH_SHORT).show();
+
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Authentication Successful", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public void methodLogin(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Authentication Successful", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Authentication Successful", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
