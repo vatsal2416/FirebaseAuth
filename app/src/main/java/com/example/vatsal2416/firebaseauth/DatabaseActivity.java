6f137385558e5acefd33379d9594f7c16b5cadaf
vatsal2416 <vatsal2416@gmail.com>
2018-08-12 10:18:16 +0530

    Added vibration when EditText is empty.

--- app/src/main/java/com/example/vatsal2416/firebaseauth/DatabaseActivity.java
+++ app/src/main/java/com/example/vatsal2416/firebaseauth/DatabaseActivity.java
@@ -1,13 +1,16 @@
 package com.example.vatsal2416.firebaseauth;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -34,6 +37,7 @@ public class DatabaseActivity extends Activity {
         final EditText textQualification = findViewById(R.id.editTextQualification);
         final EditText textAddress = findViewById(R.id.editTextAddress);
         final ListView lview = findViewById(R.id.lview);
+
         //readButton onClickListener
         readButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,19 +46,16 @@ public class DatabaseActivity extends Activity {
                 DatabaseReference myRef = database.getReference("users");
                 final List<String> list = new ArrayList<>();
                 myRef.addValueEventListener(new ValueEventListener() {
+
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         Iterable<DataSnapshot> data = dataSnapshot.getChildren();
                         for(DataSnapshot data1 : data){
                             String msg = null;
                             msg = msg +"\n"+data1.getValue();
-                            //Iterable<DataSnapshot> data2 = data1.getChildren();
-                            //for(DataSnapshot data3 : data2){
-                             //   msg = msg +"\n"+data1.getValue();
-                           // }
                             list.add(msg);
                         }
-                        textGender.setText("Reading data:");
+
                         ArrayAdapter<String> myadapter = new ArrayAdapter<String>
                                 (DatabaseActivity.this,android.R.layout.simple_list_item_1,list);
                                 lview.setAdapter(myadapter);
@@ -68,18 +69,28 @@ public class DatabaseActivity extends Activity {
             }
         });
 
+
         //insertButton onClickListener
         insertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("users");
-                DatabaseReference myRef_child = myRef.child(FirebaseAuth.getInstance().getUid());
-                myRef_child.child("Gender").setValue(textGender.getText().toString());
-                myRef_child.child("MobileNo").setValue(textMobileNo.getText().toString());
-                myRef_child.child("Qualification").setValue(textQualification.getText().toString());
-                myRef_child.child("Address").setValue(textAddress.getText().toString());
+                if (textGender.getText().toString().equals("") || textAddress.getText().toString().equals("")
+                        || textQualification.getText().toString().equals("") || textMobileNo.getText().toString().equals("")) {
+                   // textMobileNo.setText("Vibrating");
+                    Toast.makeText(DatabaseActivity.this,"You must enter value in every TextField.",Toast.LENGTH_SHORT).show();
+                    Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+                    vibrator.vibrate(200);
 
+                }else{
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("users");
+                    DatabaseReference myRef_child = myRef.child(FirebaseAuth.getInstance().getUid());
+                    myRef_child.child("Gender").setValue(textGender.getText().toString());
+                    myRef_child.child("MobileNo").setValue(textMobileNo.getText().toString());
+                    myRef_child.child("Qualification").setValue(textQualification.getText().toString());
+                    myRef_child.child("Address").setValue(textAddress.getText().toString());
+                }
             }
         });
     }
