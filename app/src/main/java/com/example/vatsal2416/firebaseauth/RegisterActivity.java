87546e3af3a490debb2298bdf1b131a9c2a51213
vatsal2416 <vatsal2416@gmail.com>
2018-08-30 18:42:29 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/vatsal2416/firebaseauth/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.vatsal2416.firebaseauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
